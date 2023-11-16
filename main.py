a32e60313126a5fd4a68e1d2e027111f82e422c3
Shant <97169131+shant-kolekar@users.noreply.github.com>
2023-11-16 01:25:45 -0500

    Update main.py with -d data/amp.txt functionality

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import getopt
 import sys
+import argparse
 
 from models.rankgan.Rankgan import Rankgan
 from utils.text_process import add_spaces
 
 
-def set_gan():
+def set_gan(input_file):
     gans = dict()
     gans['rankgan'] = Rankgan
     Gan = gans['rankgan']
@@ -16,9 +17,6 @@ def set_gan():
     gan.pre_epoch_num = 5
     gan.adversarial_epoch_num = 10
 
-    input_file = 'data/amp.txt' ############ path for input file ########################
-
-
     token_file = 'data/token.txt'
     add_spaces(input_file, token_file)
     gan_func(token_file)
@@ -27,5 +25,11 @@ def set_gan():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='GAN Training Script')
+    parser.add_argument('-d', '--data-file', type=str, default='data/amp.txt', help='Path to the input data file')
+    args = parser.parse_args()
+
+    input_file = args.data_file
+
     gan = None
-    set_gan()
+    set_gan(input_file)
